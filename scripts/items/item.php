4a56e2c76640b18db6c9e321f15ae3029b643b2a
MaximusFk <maximusfk@gmail.com>
2016-12-05 14:03:02 +0200

    Добавлены поля информации обьекта товара

--- scripts/items/item.php
+++ scripts/items/item.php
@@ -2,13 +2,17 @@
 
 class ShoesItem {
     public $name;
+    public $display_name;
     public $color = 0;
     public $secondaryColor;
+    public $category;
     public $ID;
     public $price = 0.0;
     public $price2;
     public $img_code;
+    public $img_main;
+    public $min_size;
+    public $max_size;
+    public $url;
     public $infoPairs = array("none" => "none");
 }
-
-?>
