7afb4847e8207581690ccddc93823e83e6e3fbfe
MaximusFk <maximusfk@gmail.com>
2016-12-29 11:04:14 +0200

    Добавлена страница просмотра профиля

---
+++ profile/index.php
@@ -0,0 +1,17 @@
+<?php
+require_once '../scripts/twig.php';
+require_once '../scripts/database/accounts_db.php';
+require_once '../scripts/database/orders_db.php';
+$session_id = filter_has_var(INPUT_COOKIE, 'session_id') ? filter_input(INPUT_COOKIE, 'session_id') : null;
+if($session_id && linked_to_account($session_id)) {
+    $loader = create_file_loader(['templates/profile']);
+    $twig = create_twig($loader);
+    $id = get_account_id_by_session($session_id);
+    $account = get_account_info($id);
+    $orders = get_orders_for_user($id);
+    echo $twig->render('profile_view.twig', ['account' => $account, 'orders' => $orders]);
+}
+else {
+    header("Location: /profile.php?type=login");
+}
+
