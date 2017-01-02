c42ecf6bb01660db553852356a53327001c44406
MaximusFk <maximusfk@gmail.com>
2017-01-02 09:42:57 +0200

    Добавлена проверка на входные данные

--- order.php
+++ order.php
@@ -9,14 +9,19 @@ $twig = create_twig($loader);
 
 if($method === 'GET' && (filter_has_var(INPUT_GET, 'order_id') || filter_has_var(INPUT_GET, 'access_code'))) {
     $order_id = filter_has_var(INPUT_GET, 'order_id') ? filter_input(INPUT_GET, 'order_id') : get_order_id(filter_input(INPUT_GET, 'access_code'));
-    $entries = get_entries_array($order_id, Orders);
-    foreach ($entries as &$key) {
-        $key['size_data'] = json_decode($key['size_data'], true);
-        ksort($key['size_data']);
-        $key['node'] = get_item_by_id($key['item_id']);
-    }
     $order = get_order_by_id($order_id);
-    echo $twig->render('order_view.twig', ['order_id' => $order_id, 'items' => $entries, 'order' => $order]);
+    if($order) {
+        $entries = get_entries_array($order_id, Orders);
+        foreach ($entries as &$key) {
+            $key['size_data'] = json_decode($key['size_data'], true);
+            ksort($key['size_data']);
+            $key['node'] = get_item_by_id($key['item_id']);
+        }
+        echo $twig->render('order_view.twig', ['order_id' => $order_id, 'items' => $entries, 'order' => $order]);
+    }
+    else {
+        echo $twig->render('error_page.twig', ['text' => 'Заказ не найден!']);
+    }
 }
 else if($method === 'POST') {
     $session_id = filter_input(INPUT_COOKIE, 'session_id');
@@ -28,12 +33,17 @@ else if($method === 'POST') {
     $city = filter_input(INPUT_POST, 'city');
     $office = filter_input(INPUT_POST, 'npoffice');
     $address = $city . ", " . $office;
-    $user_id = get_account_id_by_session($session_id);
-    $order_id = create_new_order($name, $surname, $email, $phone, $address, $user_id);
-    set_new_parent($order_id, Orders, $cart_id, Carts);
-    remove_cart($cart_id);
-    $access = get_access_code($order_id);
-    echo $twig->render('order_success.twig', ['order_id' => $order_id, 'access_code' => $access]);
+    if($cart_id && $name && $surname && $email && $phone && $city && $office) {
+        $user_id = get_account_id_by_session($session_id);
+        $order_id = create_new_order($name, $surname, $email, $phone, $address, $user_id);
+        set_new_parent($order_id, Orders, $cart_id, Carts);
+        remove_cart($cart_id);
+        $access = get_access_code($order_id);
+        echo $twig->render('order_success.twig', ['order_id' => $order_id, 'access_code' => $access]);
+    }
+    else {
+        echo $twig->render('error_page.twig', ['text' => 'Во время запроса произошла ошибка, пожалуйста, повторите еще раз!']);
+    }
 }
 else {
     $session_id = filter_input(INPUT_COOKIE, 'session_id');
